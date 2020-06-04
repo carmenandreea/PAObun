c5d508315bd1a70612331a50002c7c41aca4332d
carmenandreea <63059037+carmenandreea@users.noreply.github.com>
2020-06-04 21:38:02 +0300

    Update NewMain.java

--- proiect_final/main/NewMain.java
+++ proiect_final/main/NewMain.java
@@ -24,8 +24,8 @@ public class NewMain {
     public static void main(String[] args) throws Exception {
         UserService userService = UserService.getInstance();
 
-        userService.addUser(new User("Srina"));
-        userService.addUser(new User("Silvian"));
+        userService.addUser(new User("Sonia"));
+        userService.addUser(new User("Simona"));
         
 
         ArrayList<User> users = userService.getAllClients();
@@ -39,8 +39,8 @@ public class NewMain {
 
         System.out.println("\nShow all students:");
 
-        String name="Ana";
-        String description="sta aici";
+        String name="Nintendo";
+        String description="campanie de vara";
         int buget=500;
         char strategy_type;
         int becStatus=0;
